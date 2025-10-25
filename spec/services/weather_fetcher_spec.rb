414a3682cd5cf20d747b05388d128c245bb38c78
Matt McMahand <matt@invalid8.com>
2025-10-25 15:41:45 -0400

    spec fixes

--- spec/services/weather_fetcher_spec.rb
+++ spec/services/weather_fetcher_spec.rb
@@ -3,26 +3,26 @@ require 'rails_helper'
 describe "WeatherFetcher" do
   describe "fetch_forecast" do
     it "fetches the forecast" do
-       VCR.use_cassette('weather/forecast_success') do
-        result = WeatherFetcher.fetch_forecast('29732')
+      VCR.use_cassette('weather/forecast_success') do
+       result = WeatherFetcher.fetch_forecast('29732')
 
-        expected = {
-          forecast: {
-            location: "Rock Hill",
-            current_temp: 69.8,
-            forecast_days: [
-              { date: "2025-10-03", max_temp: 80.8, min_temp: 51.4 },
-              { date: "2025-10-04", max_temp: 74.7, min_temp: 57.4 },
-              { date: "2025-10-05", max_temp: 77.7, min_temp: 64.9 },
-              { date: "2025-10-06", max_temp: 77.8, min_temp: 61.5 },
-              { date: "2025-10-07", max_temp: 82.6, min_temp: 62.4 }
-            ]
-          },
-          cache_hit: false
-        }
+       expected = {
+         forecast: {
+           location: "Rock Hill",
+           current_temp: 69.8,
+           forecast_days: [
+             { date: "2025-10-03", max_temp: 80.8, min_temp: 51.4 },
+             { date: "2025-10-04", max_temp: 74.7, min_temp: 57.4 },
+             { date: "2025-10-05", max_temp: 77.7, min_temp: 64.9 },
+             { date: "2025-10-06", max_temp: 77.8, min_temp: 61.5 },
+             { date: "2025-10-07", max_temp: 82.6, min_temp: 62.4 }
+           ]
+         },
+         cache_hit: false
+       }
 
-        expect(result).to eq(expected)
-      end
+       expect(result.merge(forecast: result[:forecast].to_h)).to eq(expected)
+     end
     end
 
     it "caches calls with the same zip" do
@@ -30,9 +30,9 @@ describe "WeatherFetcher" do
       allow(Rails).to receive(:cache).and_return(cache_store)
       Rails.cache.clear
 
-       VCR.use_cassette('weather/forecast_success') do
+      VCR.use_cassette('weather/forecast_success') do
         _first_call = WeatherFetcher.fetch_forecast('29732')
-       end
+      end
 
       result = WeatherFetcher.fetch_forecast('29732')
 
@@ -47,17 +47,17 @@ describe "WeatherFetcher" do
     end
 
     it "fails to fetch the forecast" do
-       VCR.use_cassette('weather/forecast_failure') do
+      VCR.use_cassette('weather/forecast_failure') do
         result = WeatherFetcher.fetch_forecast('11111')
 
         expect(result[:error]).to eq("Error fetching forecast")
-       end
+      end
     end
 
     it "will not fetch the forecast for an invalid zipcode" do
-        result = WeatherFetcher.fetch_forecast('invalid')
+      result = WeatherFetcher.fetch_forecast('invalid')
 
-        expect(result[:error]).to eq("Invalid Zip Code")
+      expect(result[:error]).to eq("Invalid Zip Code")
     end
   end
 end
