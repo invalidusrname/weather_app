329f643dee20a192394779cba77cd94266514066
Matt McMahand <matt@invalid8.com>
2025-10-25 19:28:14 -0400

    error message cleanup

--- app/javascript/controllers/weather_fetcher_controller.js
+++ app/javascript/controllers/weather_fetcher_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
       this.inputTarget.value = zip;
     } catch (err) {
       console.error("Error:", err.message);
-      this.inputTarget.value = "Error: " + err.message;
+      this.inputTarget.value = "";
     }
   }
 
