60999977fe4421f01ce3d87378342b9a91f26912
Matt McMahand <matt@invalid8.com>
2025-10-25 19:48:33 -0400

    add default provider name

--- config/initializers/weather_fetcher.rb
+++ config/initializers/weather_fetcher.rb
@@ -1,6 +1,6 @@
 require Rails.root.join("app", "services", "weather_fetcher")
 
 WeatherFetcher.configure do |config|
-  config.provider =  ENV["WEATHER_PROVIDER"] || Rails.application.credentials.dig(:weather_api, :provider)
+  config.provider =  ENV["WEATHER_PROVIDER"] || Rails.application.credentials.dig(:weather_api, :provider) || "weather_api"
   config.api_key =  ENV["WEATHER_API_TOKEN"] || Rails.application.credentials.dig(:weather_api, :token)
 end
