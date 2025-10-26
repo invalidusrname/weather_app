c122b57886b3e5161270beca100f0cc2c67ed4a8
Matt McMahand <matt@invalid8.com>
2025-10-26 09:47:25 -0400

    minor refactoring

---
+++ .kamal/hooks/common.rb
@@ -0,0 +1,16 @@
+require "bundler/inline"
+
+gemfile { gem "octokit" }
+
+GH_REPO = "invalidusrname/weather_app"
+
+def exit_with_error(message)
+  $stderr.puts message
+  exit 0
+end
+
+def github_client
+  Octokit::Client.new(access_token: ENV["DEPLOYMENTS_GITHUB_TOKEN"])
+rescue Octokit::ClientError => error
+  exit_with_error "GitHub client error: #{error.message}"
+end
