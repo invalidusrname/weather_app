997746e7ca7b69c3509fbaa864a10196b9c3e8f3
Matt McMahand <matt@invalid8.com>
2025-11-04 22:54:01 -0500

    undo debugging

--- config/initializers/weather_fetcher.rb
+++ config/initializers/weather_fetcher.rb
@@ -3,8 +3,4 @@ require Rails.root.join("app", "services", "weather_fetcher")
 WeatherFetcher.configure do |config|
   config.provider = ENV["WEATHER_PROVIDER"] || Rails.application.credentials.dig(:weather_api, :provider) || "weather_api"
   config.api_key =  ENV["WEATHER_API_TOKEN"] || Rails.application.credentials.dig(:weather_api, :token)
-
-  puts "WeatherFetcher.configure"
-  puts "PROVIDER: #{config.provider}"
-  puts "TOKEN length: #{config.api_key.to_s.length}"
 end
